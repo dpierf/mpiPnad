00738c28e9b4eaf2356bdc4dcb7cd66ca9d8588d
Pier Francesco De Maria <dpierf@gmail.com>
2026-04-27 19:26:52 -0300

    Pacotes sem declaração explícita

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,10 @@
 #' @import data.table
 #' @import ggplot2
-#' @import scales
-#' @import shiny
-#' @import bs4Dash
-#' @import RColorBrewer
-#' @import stringr
+#' @importFrom scales label_number label_percent label_comma hue_pal percent
+#' @importFrom shiny reactive debounce eventReactive renderPlot renderUI observe req validate need HTML fluidRow column selectInput sliderInput actionButton br tags
 #' @importFrom plotly plot_ly add_trace add_segments add_markers add_bars layout ggplotly renderPlotly plotlyOutput
-#' @importFrom stats weighted.mean lm predict density sd
+#' @importFrom bs4Dash bs4DashPage bs4DashNavbar bs4DashBrand bs4DashSidebar bs4DashBody bs4DashSidebar bs4SidebarMenu bs4SidebarMenuItem bs4TabItems bs4TabItem bs4Card bs4ValueBox bs4ValueBoxOutput renderbs4ValueBox
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom stringr str_replace_all
+#' @importFrom stats lm predict weighted.mean density sd
 NULL
