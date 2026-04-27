24e9a12cb4e9df4978e8c453a0fa066b154022c0
Pier Francesco De Maria <dpierf@gmail.com>
2026-04-27 19:31:31 -0300

    Pacotes com funções não-explícitas

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 #' @import data.table
 #' @import ggplot2
 #' @importFrom scales label_number label_percent label_comma hue_pal percent
-#' @importFrom shiny reactive debounce eventReactive renderPlot renderUI observe req validate need HTML fluidRow column selectInput sliderInput actionButton br tags
+#' @importFrom shiny icon reactive debounce eventReactive renderPlot renderUI observe req validate need HTML fluidRow column selectInput sliderInput actionButton br tags
 #' @importFrom plotly plot_ly add_trace add_segments add_markers add_bars layout ggplotly renderPlotly plotlyOutput
 #' @importFrom bs4Dash bs4DashPage bs4DashNavbar bs4DashBrand bs4DashSidebar bs4DashBody bs4DashSidebar bs4SidebarMenu bs4SidebarMenuItem bs4TabItems bs4TabItem bs4Card bs4ValueBox bs4ValueBoxOutput renderbs4ValueBox
 #' @importFrom RColorBrewer brewer.pal
